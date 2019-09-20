1b3101d16c99c31b174774a919f9718774cc691b
KimaniBrian <52389639+KimaniBrian@users.noreply.github.com>
2019-09-19 22:16:45 -0400

    Gradle files

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -2,8 +2,21 @@ package com.galvanize;
 
 public class Application {
 
-    public static void main(String[]args){
+    public static void main(String[]args) {
 
         System.out.println("Hello world");
     }
+    boolean  isActive = false;
+        boolean isActive (String status){
+
+
+            if (status.equals("Active")) {
+                System.out.println("True");
+            } else if (status.equals("Inactive")) {
+                System.out.println("False");
+
+            }
+            return isActive;
+        }
+
 }
